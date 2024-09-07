adcf85b64769db6679f5b4a592fcb17f583d6503
nicovidal <nico.vidal.m@gmail.com>
2024-09-07 17:00:21 -0400

    ok

--- Proyecto/src/LicitaLabMobile.tsx
+++ Proyecto/src/LicitaLabMobile.tsx
@@ -5,7 +5,7 @@ import { View, Text } from 'react-native';
 export const LicitaLabMobile = () => {
   return (
     <View>
-        <Text>LicitaLabMobile</Text>
+        <Text>LicitaLabMobileok</Text>
     </View>
   )
 }

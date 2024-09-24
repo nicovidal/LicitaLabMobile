7bbf8ec01d861e444c6cf0ba28cfb0ebc5f03875
Matias <mgcm03@hotmail.com>
2024-09-23 23:48:55 -0300

    Ionicons

--- Proyecto/src/presentation/screens/LoginScreen.tsx
+++ Proyecto/src/presentation/screens/LoginScreen.tsx
@@ -1,11 +1,13 @@
-
 import { Text, View } from "react-native"
-
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export const LoginScreen = () => {
   return (
     <View>
         <Text>LoginScreen</Text>
+        <Icon name="rocket" size={30} color="#900"/>
+
     </View>
   )
 }
+

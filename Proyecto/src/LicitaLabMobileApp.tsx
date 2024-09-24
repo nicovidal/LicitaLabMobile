fa9ff3c110ed6a1eb177180bfd917162a0f510c2
Matias <mgcm03@hotmail.com>
2024-09-24 19:49:05 -0300

    Paper

--- Proyecto/src/LicitaLabMobileApp.tsx
+++ Proyecto/src/LicitaLabMobileApp.tsx
@@ -3,14 +3,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { View, Text } from 'react-native';
 import { StackNavigator } from './presentation/navigator/StackNavigator';
 import { BottomTabNavigator } from './presentation/navigator/BottomTabsNavigator';
+import { PaperProvider } from 'react-native-paper';
 
 
 
 export const LicitaLabMobileApp = () => {
   return (
-    <NavigationContainer>
-        {/* <StackNavigator/> */}
-        <BottomTabNavigator/>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+          {/* <StackNavigator/> */}
+          <BottomTabNavigator/>
+      </NavigationContainer>
+    </PaperProvider>
   )
 }

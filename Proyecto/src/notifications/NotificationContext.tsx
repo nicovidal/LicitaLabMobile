0ef234018a3bd36d0bfdaf12e15ad83451bb1449
nicovidal <nico.vidal.m@gmail.com>
2024-10-27 23:25:02 -0300

    avance

--- Proyecto/src/notifications/NotificationContext.tsx
+++ Proyecto/src/notifications/NotificationContext.tsx
@@ -1,18 +1,21 @@
 // notificationContext.tsx
 
-import { createContext, useEffect, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import { getNotificacions } from './getNotifications';
+import notificationService from './NotificationService';
 
 interface Notification {
-
-  id: number;
-  message: string;
-
+  id: string;
+  code: string;
+  createdAt: string;
+  read: boolean;
+  reason: string;
+  tender_id: string;
 }
 
-export const NotificationContext = createContext<Notification[]>([]); 
+export const NotificationContext = createContext<Notification[]>([]);
 
-const POLLING_INTERVAL = 5000; 
+const POLLING_INTERVAL = 5000; // Intervalo de 10 segundos
 
 export const NotificationProvider = ({ children }: { children: React.ReactNode }) => {
   const [notifications, setNotifications] = useState<Notification[]>([]);
@@ -20,10 +23,20 @@ export const NotificationProvider = ({ children }: { children: React.ReactNode }
   useEffect(() => {
     const pollerId = setInterval(async () => {
       const newNotifications = await getNotificacions();
-      if (newNotifications) setNotifications(newNotifications);
+      if (newNotifications) {
+        setNotifications(newNotifications);
+        
+        // Enviar una notificación para cada nuevo ítem
+        newNotifications.forEach((notification: Notification) => {
+          notificationService.sendNotification(
+            "Nueva notificación de licitación",
+            `ID: ${notification.tender_id} - Motivo: ${notification.reason}`
+          );
+        });
+      }
     }, POLLING_INTERVAL);
 
-    return () => clearInterval(pollerId); 
+    return () => clearInterval(pollerId);
   }, []);
 
   return (

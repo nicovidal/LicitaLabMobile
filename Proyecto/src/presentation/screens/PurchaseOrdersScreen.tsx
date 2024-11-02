c93c602934d153a58e78828486c0d5ba91c52711
Matias <mgcm03@hotmail.com>
2024-11-02 20:57:30 -0300

    Buscar Orden de compra

--- Proyecto/src/presentation/screens/PurchaseOrdersScreen.tsx
+++ Proyecto/src/presentation/screens/PurchaseOrdersScreen.tsx
@@ -1,23 +1,55 @@
-import { useEffect } from "react";
-import { StyleSheet, View, FlatList } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, View, FlatList, TextInput } from "react-native";
 import { Card, Title } from 'react-native-paper';
 import { usePurchaseOrderStore } from '../../store/purchaseOrder/usePurchaseOrderStore';
+import { MaterialIcon } from "../components/shared/MaterialIcon";
 
 export const PurchaseOrdersScreen = () => {
   const { purchaseOrder, fetchPurchaseOrder } = usePurchaseOrderStore();
+  const [searchText, setSearchText] = useState("");
+  const [filteredPurchaseOrder, setFilteredPurchaseOrder] = useState(purchaseOrder);
+
 
   useEffect(() => {
     fetchPurchaseOrder(); // Llama a la función para obtener los datos
   }, []);
 
+  const normalizeText = (text: string) => {
+    return text.normalize("NFD").replace(/[\u0300-\u036f]/g, ""); 
+  };
+
   useEffect(() => {
-    console.log("Datos de purchaseOrder (actualizados):", purchaseOrder); // Ahora debería mostrar los datos correctos
-  }, [purchaseOrder]); // Se ejecuta cada vez que purchaseOrder cambia
+    const debouncer = setTimeout(() => {
+      const normalizedSearchText = normalizeText(searchText.toLowerCase());
+      
+      const filtered = purchaseOrder.filter(opportunity => {
+        const normalizedOpportunityName = normalizeText(opportunity.name.toLowerCase());
+        const normalizedOpportunityCode = normalizeText(opportunity.code.toLowerCase());
+        
+        return normalizedOpportunityName.includes(normalizedSearchText) || 
+               normalizedOpportunityCode.includes(normalizedSearchText);
+      });
+
+      setFilteredPurchaseOrder(filtered);
+    }, 300); 
+    return () => clearTimeout(debouncer);
+  }, [searchText, purchaseOrder]);
 
   return (
     <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Buscar por ID o palabra clave"
+          autoFocus
+          autoCorrect={false}
+          value={searchText}
+          onChangeText={setSearchText}
+        />
+        <MaterialIcon name="search" size={24} color="gray" />
+      </View>
       <FlatList
-        data={purchaseOrder}
+        data={filteredPurchaseOrder}
         keyExtractor={(purchaseOrder) => purchaseOrder.code}
         numColumns={1}
         renderItem={({ item: purchaseOrder }) => (
@@ -41,6 +73,21 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 15,
     backgroundColor: '#f5f5f5',
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 10,
+    backgroundColor: "#fff",
+    paddingHorizontal: 10,
+  },
+  input: {
+    flex: 1,
+    paddingVertical: 10,
+    fontSize: 16,
+    color: '#000',
   },
   card: {
     margin: 6,

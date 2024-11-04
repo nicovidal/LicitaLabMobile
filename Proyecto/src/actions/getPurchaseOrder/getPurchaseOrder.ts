637f9d6e53ce727c0acd192a1061bdb09b97d791
Matias <mgcm03@hotmail.com>
2024-11-04 17:25:31 -0300

    Badges Orden de compra

--- Proyecto/src/actions/getPurchaseOrder/getPurchaseOrder.ts
+++ Proyecto/src/actions/getPurchaseOrder/getPurchaseOrder.ts
@@ -5,7 +5,7 @@ import { apiLicitaPurchaseOrder } from "../../config/api/api_purchase_order";
 const getPurchaseOrder = async (initialLoad: boolean) => {
   const token = await AsyncStorage.getItem('token'); 
 
-  const response = await apiLicitaPurchaseOrder.get(`/purchase-order?page=1&pageSize=10`, {
+  const response = await apiLicitaPurchaseOrder.get(`/purchase-order?page=1&pageSize=1000`, {
     headers: {
       Authorization: `Bearer ${token}` 
     }
